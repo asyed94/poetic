094afc69f1b57d13bcbfb6fddef60aca65dde435
asyed94 <asyed94@gmail.com>
2019-10-04 18:34:53 -0500

    Configures ESLint to use StandardJS rules and removes prettier.

--- config/eslint/eslint-config.js
+++ config/eslint/eslint-config.js
@@ -1,52 +1,6 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "airbnb",
-    "airbnb/hooks",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react"
-  ],
-  parserOptions: {
-    ecmaVersion: 9,
-    sourceType: "module",
-    "ecmaFeatures": {
-      "jsx": true
-    },
-  },
-  settings: {
-    "import/resolver": {
-      node: {
-        paths: ["src"],
-        extensions: [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  },
-  env: {
-    "browser": true,
-    "node": true,
-    "jest": true
-  },
-  rules: {
-    quotes: ["warn", "single", { "allowTemplateLiterals": true }],
-    "no-unexpected-multiline": "error", // https://bit.ly/2kMIX99
-  },
-  overrides: [
-    {
-      "files": ["*.js", "*.jsx"],
-      "rules": {
-        "@typescript-eslint/no-unused-vars": "off", // duplicate of by no-unused-vars
-        "@typescript-eslint/explicit-function-return-type": "off" // https://bit.ly/2kkud12
-      }
-    },
-    {
-      "files": ["*.ts", "*.tsx"],
-      "rules": {
-        "no-unused-vars": "off", // duplicate of @typescript-eslint/no-unused-vars
-        "@typescript-eslint/explicit-function-return-type": ["error"] // https://bit.ly/2kkud12
-      }
-    }
-  ]
+  "extends": "standard-with-typescript",
+  "parserOptions": {
+      "project": "./tsconfig.json"
+  }
 };
