239d1c9e1c25530147fe261835cbef05e4dfa1e1
asyed94 <asyed94@gmail.com>
2019-10-04 19:04:45 -0500

    Added ESLint rules to turn off 'no-console' and 'no-restricted-syntax' rules

--- config/eslint/eslint-config.js
+++ config/eslint/eslint-config.js
@@ -2,5 +2,9 @@ module.exports = {
   "extends": "standard-with-typescript",
   "parserOptions": {
       "project": "./tsconfig.json"
+  },
+  rules: {
+    "no-console": "off",
+    "no-restricted-syntax": "off"
   }
 };
